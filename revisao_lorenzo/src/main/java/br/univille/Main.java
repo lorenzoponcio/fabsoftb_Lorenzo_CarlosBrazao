5fe6a9a28fb0388b4234bcc3e6ae426112d2204a
lorenzoponcio <lorenzoponcio99@gmail.com>
2025-03-24 22:01:48 -0300

    Feat - biblioteca swing

--- revisao_lorenzo/src/main/java/br/univille/Main.java
+++ revisao_lorenzo/src/main/java/br/univille/Main.java
@@ -1,15 +1,48 @@
 package br.univille;
 
+import br.univille.entity.Cidade;
+import br.univille.entity.Cliente;
+import br.univille.entity.Pokemon;
+
 public class Main {
     public static void main(String[] args) {
-        // System.out.println("Carlos");
+        //System.out.println("Hello world!");
 
-        Cliente carlos = new Cliente("carluxo");
-        Cliente lolo = new Cliente("lorenzo");
+        Cidade joinville = new Cidade();
+        joinville.setNome("Joinville");
+        joinville.setEstado("Santa Catarina");
+        
+        Pokemon jigglypuff = new Pokemon("Jigglypuff");
+        Pokemon porygon = new Pokemon("Prygon");
 
-        System.out.println(carlos.getNome() + " " + lolo.getNome());
-        System.out.println(carlos.toString());
-        System.out.println(lolo.hashCode());
-        System.out.println(carlos.getNome().equals(lolo.getNome()));
+        Cliente zezinho = new Cliente("Zezinho");
+        zezinho.setNome("Zezinho da silva Sauro");
+        zezinho.setCidade(joinville);
+
+        zezinho.getListaPokemon().add(jigglypuff);
+        zezinho.getListaPokemon().add(porygon);
+        // zezinho.getListaPokemon().add(123);
+        // zezinho.getListaPokemon().add(2.2);
+        // zezinho.getListaPokemon().add("fds pokemon");
+        // zezinho.getListaPokemon().add(false);
+
+        for(int i=0;i<zezinho.getListaPokemon().size();i++){
+            var umPokemon = zezinho.getListaPokemon().get(i);
+            System.out.println(umPokemon);
+        }
+
+        for(var umPokemon : zezinho.getListaPokemon()){
+            System.out.println(umPokemon);
+        }
+
+        zezinho.getListaPokemon().stream().forEach(System.out::println);
+
+
+        Cliente mariazinha = new Cliente();
+        mariazinha.setNome("Mariazinha");
+
+
+        System.out.println(zezinho);
+        System.out.println(mariazinha);
     }
 }
