9a0e2e69874308abae087a25a3394aa820c9901b
lorenzoponcio <lorenzoponcio99@gmail.com>
2025-04-01 20:24:31 -0300

    Início do Projeto

---
+++ projetofabsoftmp/src/main/java/br/univille/projetofabsoftmp/ProjetofabsoftmpApplication.java
@@ -0,0 +1,13 @@
+package br.univille.projetofabsoftmp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetofabsoftmpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetofabsoftmpApplication.class, args);
+	}
+
+}
---
+++ projetofabsoftmp/src/test/java/br/univille/projetofabsoftmp/ProjetofabsoftmpApplicationTests.java
@@ -0,0 +1,13 @@
+package br.univille.projetofabsoftmp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetofabsoftmpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
